c9fb135d64d9ebaf803e3c3111c1da00d94f94f0
andrei <andriy.kharkhan@gmail.com>
2022-05-09 01:21:45 +0300

    HomeWord Done + extra

--- tranings/Gamma_Traning/GammaTraining_2022-05-05.py
+++ tranings/Gamma_Traning/GammaTraining_2022-05-05.py
@@ -1,1 +1,24 @@
-a=12
+a = 15
+b = 2
+# print(a, type(a), b, type(b))
+# c = a + b
+# print(c, type(c))
+# text = 'bugaga'
+
+# print(str(a) + text)
+# print \
+#    (a / b)
+##print(a // b)
+print(a % b)
+# _a = text[2]
+# print(_a)
+# asddd = None
+# print(asddd)
+# print("""rent""")
+
+storon_A = float(input('Please insert side A: '))
+storon_B = float(input('Please insert side B: '))
+storon_C = float(input('Please insert side C: '))
+poluperriment = (storon_A + storon_B + storon_C) / 2
+ploshadj = (poluperriment * (poluperriment - storon_A) * (poluperriment - storon_B) * (poluperriment - storon_C)) ** 0.5
+print('Прощадь треугольника: '+ str(ploshadj))
