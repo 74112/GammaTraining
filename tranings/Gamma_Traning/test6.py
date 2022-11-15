4187ef40e73b5d9957464129adbc36aeb1a9667a
74112 <87581144+74112@users.noreply.github.com>
2022-11-16 00:49:01 +0200

    Delete test6.py

--- tranings/Gamma_Traning/test6.py
+++
@@ -1,8 +0,0 @@
-kortez_1 = (1, 2, 32, 6565)
-kortez_2 = (15, 211.2)
-print(kortez_1)
-kortez_1 = list(kortez_1[:2]) + list(kortez_2) + list(kortez_1[2:])
-
-kortez_1 = tuple(kortez_1)
-print(type(kortez_1))
-print(kortez_1)
